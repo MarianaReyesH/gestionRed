6597eb6407ef26583f4a61b95a07a3440efb650e
Mariana Reyes <marreyhen@alum.us.es>
2024-05-02 18:01:02 +0200

    Fallo WIH - el resultado del wih ping es null
    En la implementación del wih ping se devuelve un Map<String, Object>, pero en el proceso se recibe el resultado como null

---
+++ gestionRed-service/src/main/java/us/dit/gestionRed/service/services/kie/handlers/pingWIH.java
@@ -0,0 +1,34 @@
+package us.dit.gestionRed.service.services.kie.handlers;
+
+
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.kie.api.runtime.process.WorkItem;
+import org.kie.api.runtime.process.WorkItemHandler;
+import org.kie.api.runtime.process.WorkItemManager;
+import org.springframework.stereotype.Component;
+
+@Component("ping")
+public class pingWIH implements WorkItemHandler {
+	private static final Logger logger = LogManager.getLogger();
+
+	@Override
+	public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
+		logger.info("Se está ejecutando pingWIH con los detalles de workItem " + workItem);
+		
+		Map<String,Object> parametros = workItem.getParameters();
+		logger.info("Dirección IP a comprobar: "+(String)parametros.get("dirIP"));
+		Map<String,Object> resultados = Map.of("accesoMaquina", true);
+		logger.info("Resultado del ping: "+ resultados.get("accesoMaquina"));
+		manager.completeWorkItem(workItem.getId(), resultados);
+	}
+
+	@Override
+	public void abortWorkItem(WorkItem workItem, WorkItemManager manager) {
+
+	}
+
+}
+
